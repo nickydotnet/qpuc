ba175287e5c08e259a8eae1c9719be84958a0803
nickydotnet <contact@nicolas-denis.com>
2024-05-30 11:04:43 +0200

    footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -5,12 +5,7 @@ import { FooterContainer, Repository, Credits, GitHub } from './style'
 function Footer() {
   return (
     <FooterContainer>
-      <Credits>Made by Evan Jannot</Credits>
-      <Repository>
-        <GitHub to="//github.com/EvanJannot/QPUC" $isFullLink target="_blank">
-          Check the project's GitHub
-        </GitHub>
-      </Repository>
+      
     </FooterContainer>
   )
 }
